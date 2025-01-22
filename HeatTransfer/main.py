6e95ca911b2e87e33c25e08cb6624fe0b1a0848e
Jakub Zając <jakubtomaszkrzysztof@gmail.com>
2025-01-22 22:31:26 +0100

    Edited plot image

--- HeatTransfer/main.py
+++ HeatTransfer/main.py
@@ -3,5 +3,7 @@ from plot import show
 
 if __name__ == "__main__":
     n = int(input("Podaj liczbę elementów siatki: "))
+    if n <= 0:
+        raise ValueError("Liczba elementów siatki musi być większa od zera")
     x, y = solve(n)
     show(x, y, n)
